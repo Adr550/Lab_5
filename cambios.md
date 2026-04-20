fde98153e2783a81f452f86c5cfcb19eb7d90312
Chaostheory05 <luisbancesestrada@gmail.com>
2026-04-19 19:11:17 -0600

    Refactor 200 to 404

--- cambios.md
+++ cambios.md
@@ -2,3 +2,5 @@ Cambios realizados:
 Se añadió await en la función asincrona en /api/student
 
 Se cambió application-json a application/json ya que se encontraba en un formato incorrecto
+
+Se cambió el request de 200 a 404 
--- servidor-malo.js
+++ servidor-malo.js
@@ -12,7 +12,7 @@ const server = http.createServer(async (req, res) => {
   }
 
   if (req.url === "/info") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(404, { "Content-Type": "application/json" });
     res.end("Ruta de información");
     return
   }
@@ -25,7 +25,7 @@ const server = http.createServer(async (req, res) => {
     return
   }
 
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(404, { "Content-Type": "text/plain" });
   res.end("Ruta no encontrada");
 })
 
